df466a3750c515934df0d79c50c52d0d22b23db4
codewithgyan467 <duwarahgyandeep248@gmail.com>
2024-10-28 16:18:45 +0530

    ne

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,18 +3,15 @@ import Home from './components/Home';
 import ServiceProviderProfile from './components/ServiceProviderProfile';
 import AdminDashboard from './components/AdminDashboard';
 import UserProfile from './components/UserProfile';
-import SignIn from './components/SignIn';
-import Register from './components/SignUp'; // Import Register component
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<SignIn />} />
-        <Route path="/sign-up" element={<Register />} /> 
+       
         <Route path="/home" element={<Home />} /> 
         <Route path="/provider/:id" element={<ServiceProviderProfile />} />
-        <Route path="/admin" element={<AdminDashboard />} />
+        <Route path="/" element={<AdminDashboard />} />
         <Route path="/user-profile" element={<UserProfile />} />
       </Routes>
     </Router>
