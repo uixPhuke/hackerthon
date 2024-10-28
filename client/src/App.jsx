b61ae72ae2518869f7836e58002ad5acd516b359
codewithgyan467 <duwarahgyandeep248@gmail.com>
2024-10-28 15:48:02 +0530

    service provider data

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,18 @@
-
-
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './components/Home'; // Correct import path
+import Home from './components/Home';
 import ServiceProviderProfile from './components/ServiceProviderProfile';
 import AdminDashboard from './components/AdminDashboard';
 import UserProfile from './components/UserProfile';
+import SignIn from './components/SignIn';
+import Register from './components/SignUp'; // Import Register component
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Home />} /> {/* Home component on root path */}
+        <Route path="/" element={<SignIn />} />
+        <Route path="/sign-up" element={<Register />} /> 
+        <Route path="/home" element={<Home />} /> 
         <Route path="/provider/:id" element={<ServiceProviderProfile />} />
         <Route path="/admin" element={<AdminDashboard />} />
         <Route path="/user-profile" element={<UserProfile />} />
