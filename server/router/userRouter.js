349a3212dadd6fdffaa75ede0d27487013aa0e4a
Rohan Phu-ke <uixphuke@gmail.com>
2024-10-28 14:09:13 +0530

    controller,router,model set

---
+++ server/router/userRouter.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const userCtrl = require("../controller/userCtrl");
+
+const router = express.Router();
+
+// User routes
+router.post("/register", userCtrl.registerUser);
+router.post("/login", userCtrl.loginUser);
+router.post("/refresh_token", userCtrl.refreshtoken);
+router.get("/", userCtrl.getUsers);
+router.get("/:id", userCtrl.getUserById);
+router.put("/:id", userCtrl.updateUser);
+router.delete("/:id", userCtrl.deleteUser);
+router.post("/google", userCtrl.signInGoogle);
+router.post("/logout", userCtrl.logoutUser);
+
+module.exports = router;
--- server/server.js
+++ server/server.js
@@ -1,14 +1,26 @@
 const express=require('express')
 const app=express()
+const cookieParser = require("cookie-parser");
+const userRouter = require("./router/userRouter");
 require('dotenv').config()
 const connectDB=require('./db')
 
+//connect to database
 connectDB()
 
+//server 
 app.get('/',(req,res)=>{
 res.json({message:"This is Samrtcity server"})
 })
 
+// Middleware
+app.use(express.json());
+app.use(cookieParser());
+
+// Use user routes
+app.use("/user", userRouter);
+
+//PORT
 const PORT=process.env.PORT 
 
 app.listen(PORT,()=>{
